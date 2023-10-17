b17bd67b0560083e6dc06a8e4c68415ac7894bee
Abel Naharro <abelnhm92@gmail.com>
2023-10-18 00:23:15 +0200

    Add function strictEquals and call to function

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import { strictEquals } from './js/tools.js';
 
-strictEquals();
+console.log('A', '|', 'B', '|', 'RESULT', '|', '');
+
+strictEquals(1, 1);
+strictEquals(NaN, NaN);
+strictEquals(0, -0);
+strictEquals(-0, 0);
+strictEquals(1, '1');
+strictEquals(true, false);
+strictEquals(false, false);
+strictEquals('water', 'oil');
